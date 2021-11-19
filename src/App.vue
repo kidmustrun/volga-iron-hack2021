7d2f2ad7902e66b5de55b10f84aeb8dbabc1b3ad
Irina <parakerta@mail.ru>
2021-11-19 22:46:58 +0300

    header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.js"
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand d-flex flex-column align-items-center" href="#">
+          <img src="@/assets/logo.png" alt="logo" height="60" />
+          <span class="logo-text">Каспийский лотос</span>
+        </a>
+         
+              <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <div class="nav-item">
+            <a class="nav-link pt-0 pb-0" aria-current="page" href="#"
+                >8-927-284-41-93</a
+              >
+              <a class="nav-link pt-0 pb-0" href="#">soc_zinovevana@pvrr.ru</a>
+              </div>
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Домики</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Мероприятия</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Контакты</a>
+            </li>
+          </ul>
+          <button class="btn_border">Забронировать</button>
+        </div>
+        
+         
+              
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    msg: String,
+  },
+};
+</script>
+<style scoped>
+.navbar_set {
+  height: 100px;
+  background-color: #322d3a;
+}
+nav a {
+  color: white !important;
+  font-size: 20px;
+}
+.logo-text {
+  color: #fdd704;
+}
+.btn_border {
+  border: solid 3px #fdd704;
+  background: transparent;
+  color: white;
+  padding: 5px 15px;
+  font-size: 20px;
+  margin: 0px 10px;
+}
+</style>
