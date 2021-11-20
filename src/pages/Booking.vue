d1b7eb58af911172124fb47f810c9dc388a99c8e
Mandarinka450 <nastya-s-01@mail.ru>
2021-11-20 17:57:12 +0300

    done booking page

--- src/pages/Booking.vue
+++ src/pages/Booking.vue
@@ -15,21 +15,56 @@
       </form>
       <button class="btn_border mt-5">Найти</button>
     </div>
+    <h4 class="date">23 декабря -26 декабря</h4>
+    <div class="select_houses">
+      <h5 class="text-center">Выберите домик</h5>
+      <div class="row">
+               <div class="col-sm">
+                  <img src="@/assets/room2.jpeg" />
+                  <div class="info">
+                    <p class="title">Двухместный домик</p>
+                    <div class="price">
+                      <p class="price_box">от 10 000 руб</p>
+                      <router-link class="btn_border align-right mt-3" to="/booking">
+                        Выбрать
+                      </router-link>
+                    </div>
+                  </div>
+               </div>
+              <div class="col-sm">
+                 <img src="@/assets/room1.jpeg" />
+                 <div class="info">
+                    <p class="title">Четырехместный домик</p>
+                    <div class="price">
+                      <p class="price_box">от 15 000 руб</p>
+                      <router-link class="btn_border align-right mt-3" to="/booking">
+                        Выбрать
+                      </router-link>
+                    </div>
+                  </div>
+              </div>
+          </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Booking",
+
 };
 </script>
 
 <style>
 .container-form {
   background-color: #fbfbfb;
+  box-shadow: 0px 4px 6px 0px rgba(0, 0, 0, 0.1);
 }
 input{
     width: 70%;
+    border: 1px solid rgba(162, 171, 177, 0.7);
+    height: 6vh;
+    padding: 2%;
 }
 .btn_border {
   padding: 12px 10%;
@@ -42,5 +77,81 @@ input{
 .btn_border:hover {
   background: #b89e40;
   transition: ease 0.5s;
+}
+.date{
+  margin-top: 13vh;
+  font-weight: bold;
+  font-size: 24px;
+}
+h5{
+  margin-top: 8vh;
+  font-weight: bold;
+  font-size: 24px;
+}
+.row{
+   margin-top: 7vh;
+    display: flex;
+    align-items: center;
+    row-gap: 3vh;
+    column-gap: 5vh;
+}
+.row img {
+  margin:auto auto;
+  max-width: 100%;
+  height: auto;
+  max-height: 70%;
+  display: block;
+}
+.info{
+  padding-right: 3vh;
+  padding-left: 5vh;
+  padding-top: 3vh;
+  height: 21vh;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+
+}
+.title{
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+.price{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.btn_border {
+  margin-top: 0;
+  padding: 8px 23px;
+  border: none;
+  color: white;
+  background: #C9B158;
+  font-size: 18px;
+  text-decoration: none;
+  transition: ease 0.5s;
+}
+.btn_border:hover {
+  border: none;
+  color: #fff;
+  background: #322d3a;
+  transition: ease 0.5s;
+}
+.price_box{
+  font-weight: bold;
+  font-size: 24px;
+}
+@media (max-width: 760px){
+  .price{
+    flex-direction: column;
+    justify-content: center;
+  }
+  .row{
+    flex-direction: column;
+  }
+  .info{
+    height: 26vh;
+  }
+  .title, .date{
+    text-align: center;
+  }
 }
 </style>
